66866fde2cf7cc472276f070403d799b95171fcb
novodantis <novodantis@gmail.com>
2013-12-14 21:04:25 +0000

    Lith and flythru control

---
+++ Assets/Scripts/controls/FlythroughControl.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlythroughControl : MonoBehaviour {
+
+	#region Public Members
+	public float movingSpeed = 5.0f;
+	public float turnSpeed = 5.0f;
+	#endregion
+
+	#region Private Members
+	private float moveX;
+	private float moveY;
+	private float turnX;
+	private float turnY;
+	#endregion
+	
+	void Update () 
+	{
+		moveX = Input.GetAxis("Horizontal") * movingSpeed;
+		moveY = Input.GetAxis("Vertical") * movingSpeed;
+
+		turnX += Input.GetAxis("Mouse X") * turnSpeed * Time.deltaTime;
+		turnY += Input.GetAxis("Mouse Y") * turnSpeed * Time.deltaTime;
+
+		//transform.Translate(moveX, 0, moveY);
+		transform.eulerAngles = new Vector3(turnY, turnX, 0);
+	}
+
+	void FixedUpdate ()
+	{
+		rigidbody.AddRelativeForce(moveX, 0, moveY);
+	}
+}
